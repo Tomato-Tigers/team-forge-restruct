93f91fdf7428d95b6a8690eb4e6f1bca00400c0b
xavii-p <xavier.piercee@gmail.com>
2023-11-09 19:49:06 -0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+
 
 import Login from "./components/Login";
 import Register from "./components/Register";
@@ -7,36 +9,57 @@ import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Messages from "./components/Messages";
 import ClassPage from "./components/ClassPage";
+import MainLayout from "./components/MainLayout";
 
-const classes = [
-  {
-    id: "1",
-    title: "CS 370",
-    subtitle: "Computer Science Practicum",
-    members: 17,
-  },
-  {
-    id: "2",
-    title: "CS 329",
-    subtitle: "Computational Linguistics",
-    members: 5,
-  },
-];
+interface User{
+  name: string;
+  email: string;
+}
+
 
 const App: React.FC = () => {
+  const [user, setUser] = useState <User>({name: "", email: ""});
+  
+
+  const handleLogin = (user: User) => {
+    setUser(user);
+  };
+
+
+    
+  const handleLogout = () => {
+    setUser({name: "", email: ""});
+  }
+
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Login onLogin = {handleLogin}/>} />
           <Route path="/RQTest" element={<RQTest />} />
           <Route path="/Register" element={<Register />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/Projects" element={<Projects classes={classes} />}>
+
+          <Route path="/Home" element={
+              
+                <MainLayout user={user} onLogout={handleLogout} >
+                  <Home user = {user} onLogout={handleLogout} />
+                </MainLayout> } />
+
+          <Route path="/Projects" element={
+
+              <MainLayout user={user} onLogout={handleLogout} >
+                <Projects user = {user} onLogout = {handleLogout} />
+              </MainLayout> }>
+
             <Route index element={<ClassPage />} />
             <Route path=":id" element={<ClassPage />} />
           </Route>
-          <Route path="/Messages" element={<Messages />} />
+          <Route path="/Messages" element={
+            
+              <MainLayout user={user} onLogout={handleLogout}>
+                <Messages user={user} onLogout={handleLogout} />
+            </MainLayout> 
+          } />
         </Routes>
       </Router>
     </div>
