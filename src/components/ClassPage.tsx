d07fbce0c24b0cfaa1d49a7ed54281fd036d3694
xavii-p <xavier.piercee@gmail.com>
2023-11-20 00:05:11 -0500

    Update ClassPage.tsx

--- src/components/ClassPage.tsx
+++ src/components/ClassPage.tsx
@@ -31,33 +31,76 @@ interface ClassPageProps {
 
 const ClassPage: React.FC<ClassPageProps> = ({ user, onLogout }) => {
   const { classID } = useParams<{ classID: string }>();
+
+  const location = useLocation();
+  const subtitle = location.state.subtitle|| "Class Page";
+
+  
   const [projects, setProjects] = React.useState<Project[]>([]);
   const [newProjectTitle, setNewProjectTitle] = useState<string>("");
   const [newProjectDesc, setNewProjectDesc] = useState<string>("");
   const [newGroupName, setNewGroupName] = useState<string>("");
-  axios
-    .post("/api/createProject", {
-      email: user?.email,
-      title: newProjectTitle,
-      description: newProjectDesc,
-      classID: classID,
-      groupName: newGroupName,
-    })
-    .then((res) => {
-      setProjects((prevProjects) => [
-        ...prevProjects,
-        {
-          title: newProjectTitle,
-          description: newProjectDesc,
-          members: [user.name],
-        },
-      ]);
-      setNewProjectTitle("");
-      setNewProjectDesc("");
-    })
-    .catch((error) => {
-      console.error(`Error creating project: ${error.message}`);
-    });
+
+  useEffect(() => {
+    if (classID) {
+       axios.post("http://localhost:3001/getProjects", {
+            classID: classID
+          })
+          .then((res) => {
+            setProjects(res.data);
+          })
+          .catch((error) => {
+          // alert(`Error fetching classes: ${error.message}`);
+            console.error(`Error fetching projects: ${error.message}`);
+          });}
+  }, []);
+
+  
+  const handleNewProjectTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewProjectTitle(e.target.value);
+  };
+  
+  const handleNewProjectDescChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewProjectDesc(e.target.value);
+  };
+
+  const handleNewGroupNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewGroupName(e.target.value);
+  }
+
+  const handleNewProjectSubmission = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (newProjectTitle === "" || newProjectDesc === "") {
+      setErrorMessage("Please fill out both fields.");
+      return;
+    }
+    axios.post("/api/createProject", {
+        email: user?.email,
+        title: newProjectTitle,
+        description: newProjectDesc,
+        classID: classID,
+        groupName: newGroupName,
+      })
+      .then((res) => {
+        setProjects((prevProjects) => [
+          ...prevProjects,
+          {
+            title: newProjectTitle,
+            description: newProjectDesc,
+            members: [user?.name],
+          },
+        ]);
+        setNewProjectTitle("");
+        setNewProjectDesc("");
+        setShowForm(false);
+      })
+      .catch((error) => {
+        setErrorMessage(`Error creating project: ${error.message}`);
+        console.error(`Error creating project: ${error.message}`);
+      });
+  };
+
+  
   return (
     <MainLayout user={user} onLogout={onLogout}>
       <div className="class-page">
