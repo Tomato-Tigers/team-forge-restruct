b9680862d3c93ac0c6e466a4b8903f796c5d988a
xavii-p <xavier.piercee@gmail.com>
2023-11-19 21:05:10 -0500

    Add files via upload

---
+++ api/getProjects.js
@@ -0,0 +1,27 @@
+//API to get all projects specific to a class
+
+module.exports = (async (req, res) => {
+    const data = req.body;
+    const classID = data.classID;
+    console.log("classID: ", classID);
+    try {
+        const projects = await prisma.class.findUnique({
+            where: {
+                classID: classID,
+            },
+            select: {
+                projects: true,
+            },
+        });
+        // send empty array if no projects found
+        if (!projects) {
+            return res.json([]);
+        } else {
+            console.log("Projects: ", projects.projects);
+            return res.json(projects.projects);
+        }
+    } catch (error) {
+        console.error("Error getting projects:", error);
+        return res.status(500).send('Internal server error');
+    }
+});
