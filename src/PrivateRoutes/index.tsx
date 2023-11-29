872d754fac2e7c73bf5b057c65ff2a3603af7ec4
nicholasueki <nicholas.ueki@gmail.com>
2023-11-29 01:42:12 -0500

    jwt-auth implementation

---
+++ src/PrivateRoutes/index.tsx
@@ -0,0 +1,16 @@
+import useLocalState from "../components/useLocalStorage.js";
+import{Navigate} from "react-router-dom";
+import React, { ReactNode, ReactElement } from 'react';
+
+
+
+interface PrivateRouteProps {
+  children: ReactNode;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }): ReactElement | null => {
+  const [jwt, setJwt] = useLocalState("", "jwt");
+  return jwt ? <>{children}</> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
