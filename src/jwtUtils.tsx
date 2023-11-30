8ec403b424b6dfae21aec0f5b6aaac34ae4dd8f2
nicholasueki <nicholas.ueki@gmail.com>
2023-11-30 17:06:16 -0500

    bug fixing jwtUtils function.

--- src/jwtUtils.tsx
+++ src/jwtUtils.tsx
@@ -1,16 +1,19 @@
-const jwt_decode = require('jwt-decode');
+ 
+const jwtDecode = require('jwt-decode');
 
 
 interface DecodedToken {
   name: string;
- 
+  
 }
 
 export const getUsernameFromJWT = (): string | null => {
   const token = localStorage.getItem('jwt');
+  console.log('JWT Token:', token); // Log the token for debugging
+
   if (token) {
     try {
-      const decoded: DecodedToken = jwt_decode(token);
+      const decoded: DecodedToken = jwtDecode(token);
       return decoded.name;
     } catch (error) {
       console.error('Error decoding token:', error);
