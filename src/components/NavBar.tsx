65fe8fe76710f198a9ced4b7be94bbdfa47206c9
xavii-p <xavier.piercee@gmail.com>
2023-12-01 06:07:00 -0500

    Add files via upload

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,24 @@
-/*
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import "./NavBar.css";
 import image from "./Logo.png";
 import menuIcon from "./menu.png";
 
+
 interface User {
-  name: string;
-  email: string;
-}
+    name: string;
+    email: string;
+  }
 
-interface NavBarProps {
-  user: User;
-  onLogout: () => void;
-}
+  interface NavBarProps{
+    user: User;
+    // navigate to login page on logout
+    onLogout: () => void;
+  }
 
-const NavBar: React.FC<NavBarProps> = ({ user, onLogout }) => {
+const NavBar: React.FC<NavBarProps> = ({user, onLogout}) => {
   const [showDropdown, setShowDropdown] = useState(false);
 
   const navigate = useNavigate();
@@ -30,25 +32,29 @@ const NavBar: React.FC<NavBarProps> = ({ user, onLogout }) => {
     setShowDropdown(!showDropdown);
   };
 
+  const handleLogoClick = () => {
+    navigate("/Home");
+  };
   return (
     <div className="navbar">
       <div className="logo">
-        <img src={image} alt="Logo" />
+        <img src={image} alt="Logo" onClick={handleLogoClick} />
+      </div>
+      <div className="branding">
+        <span className="blue_text">Team</span>Forge
       </div>
       <div className="links">
         <Link to="/Home">Home</Link>
         <Link to="/Messages">Messages</Link>
         <Link to="/Projects">Projects</Link>
       </div>
-     <div className="menuIcon">
-        < img src={menuIcon} alt="Menu" onClick={toggleDropdown} />
+      <div className="menuIcon">
+      <img src={menuIcon} alt = "Menu" onClick={toggleDropdown} />
       </div>
       {showDropdown && (
-        <div className={`dropdown ${showDropdown ? "animate" : ""}`}>
+        <div className={`dropdown ${showDropdown ? 'animate': ''}`}>
           <p>Hello, {user.name}!</p>
-          <button className="sign-out-button" onClick={handleLogout}>
-            Sign Out
-          </button>
+          <button className="sign-out-button" onClick = {handleLogout}>Sign Out</button>
         </div>
       )}
     </div>
@@ -56,69 +62,3 @@ const NavBar: React.FC<NavBarProps> = ({ user, onLogout }) => {
 };
 
 export default NavBar;
-*/
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-
-import "./NavBar.css";
-import image from "./Logo.png";
-import menuIcon from "./menu.png";
-
-interface User {
-  name: string;
-  email: string;
-}
-
-interface NavBarProps {
-  user: User;
-  onLogout: () => void;
-}
-
-const NavBar: React.FC<NavBarProps> = ({ user, onLogout }) => {
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    onLogout();
-    navigate("/");
-  };
-
-  const toggleDropdown = () => {
-    setShowDropdown(!showDropdown);
-  };
-  const navigateToHomepage = () => {
-    navigate("/Home");
-  };
-  const navigateToProfile = () => {
-    navigate("/ProfilePage");
-  }
-
-  return (
-    <div className="navbar">
-       <div className="logo" onClick={navigateToProfile}>
-        <img src={image} alt="Logo" />
-      </div>
-      <div className="links">
-        <Link to="/Home">Home</Link>
-        <Link to="/Messages">Messages</Link>
-        <Link to="/Projects">Projects</Link>
-      </div>
-     <div className="menuIcon">
-        < img src={menuIcon} alt="Menu" onClick={toggleDropdown} />
-      </div>
-      {showDropdown && (
-        <div className={`dropdown ${showDropdown ? "animate" : ""}`}>
-          <p>
-  Hello, <span onClick={navigateToHomepage}>{user.name}</span>!
-</p>
-          <button className="sign-out-button" onClick={handleLogout}>
-            Sign Out
-          </button>
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default NavBar; 
