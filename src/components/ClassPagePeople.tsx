94f161723388db6a782989b3b3f3b45aef6d8800
Henry Mitchell <henry.mitchell0903@gmail.com>
2023-12-01 06:57:39 -0500

    Frontend for grouping algorithm.

--- src/components/ClassPagePeople.tsx
+++ src/components/ClassPagePeople.tsx
@@ -5,10 +5,11 @@ import "./Projects.css";
 import MainLayout from "./MainLayout";
 import "./ClassPage.css";
 import "./ClassPageNavBar.css";
-import "./ClassPagePeople.css"
+import "./ClassPagePeople.css";
 
 import { useParams } from "react-router-dom";
 import ClassPageNavBar from "./ClassPageNavBar";
+import { listenerCount } from "process";
 
 // const Search = require("./../api/utils/_search.js");
 
@@ -39,15 +40,12 @@ interface ClassPagePeopleProps {
   onLogout: () => void;
 }
 
-const Test: React.FC<ClassPagePeopleProps> = ({
-  user,
-  onLogout,
-}) => {
+const Test: React.FC<ClassPagePeopleProps> = ({ user, onLogout }) => {
   const { classID } = useParams<{ classID: string }>();
   const [profiles, setProfiles] = useState<Profile[]>([]);
   const [relation, setRelation] = useState<string[]>([]);
   var x = 0;
-  const [, forceUpdate] = useReducer(x => x + 1, 0);
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
 
   // useEffect(() => {
   //   if (user?.email) {
@@ -68,12 +66,12 @@ const Test: React.FC<ClassPagePeopleProps> = ({
   var pref = {
     interest: 5,
     skill: 5,
-    hasSki: [],     // skills that must have
-    hasInt: [],     // interests that must have
-    notSki: [],     // skills that must not have
-    notInt: [],     // interests that must not have
-    maySki: [],     // skills prefered
-    mayInt: []      // interests prefered
+    hasSki: [], // skills that must have
+    hasInt: [], // interests that must have
+    notSki: [], // skills that must not have
+    notInt: [], // interests that must not have
+    maySki: [], // skills prefered
+    mayInt: [], // interests prefered
   };
 
   if (profiles.length === 0) {
@@ -81,188 +79,171 @@ const Test: React.FC<ClassPagePeopleProps> = ({
     // setPeople(Search.search(people, user.id, pref));
     setProfiles([
       {
-        "id": "1",
-        "name": "Person 1",
-        "email": "123@gmail.com",
-        "skills": [
+        id: "1",
+        name: "Person 1",
+        email: "123@gmail.com",
+        skills: [
           "Java",
           "Placeholder",
           "Placeholder",
           "Placeholder",
           "Placeholder",
         ],
-        "interests": [
+        interests: [
           "music",
           "Placeholder",
           "Placeholder",
           "Placeholder",
           "Placeholder",
         ],
-        "availability": [
-          ";08:00-9:00;10:00-13:00", "", "", "", "", "", ""
-        ],
-        "relation": ["2"]
-      }, {
-        "id": "2",
-        "name": "Person 2",
-        "email": "123@gmail.com",
-        "skills": [
-          "Java"
-        ],
-        "interests": [
-          "food"
-        ],
-        "availability": [
-          ";08:00-9:00;10:00-13:00", "", "", "", "", "", ""
-        ],
-        "relation": []
+        availability: [";08:00-9:00;10:00-13:00", "", "", "", "", "", ""],
+        relation: ["2"],
       },
       {
-        "id": "3",
-        "name": "Person 3",
-        "email": "123@gmail.com",
-        "skills": [
-          "graphic design"
-        ],
-        "interests": [
-          "food"
-        ],
-        "availability": [
-          ";08:00-9:00;10:00-13:00", "", "", "", "", "", ""
-        ],
-        "relation": ["2"]
+        id: "2",
+        name: "Person 2",
+        email: "123@gmail.com",
+        skills: ["Java"],
+        interests: ["food"],
+        availability: [";08:00-9:00;10:00-13:00", "", "", "", "", "", ""],
+        relation: [],
       },
       {
-        "id": "4",
-        "name": "Person 4",
-        "email": "123@gmail.com",
-        "skills": [
-          "C"
-        ],
-        "interests": [
-          "biking"
-        ],
-        "availability": [
-          ";08:00-9:00;10:00-13:00", "", "", "", "", "", ""
-        ],
-        "relation": []
+        id: "3",
+        name: "Person 3",
+        email: "123@gmail.com",
+        skills: ["graphic design"],
+        interests: ["food"],
+        availability: [";08:00-9:00;10:00-13:00", "", "", "", "", "", ""],
+        relation: ["2"],
       },
       {
-        "id": "5",
-        "name": "Person 4",
-        "email": "123@gmail.com",
-        "skills": [
-          "C"
-        ],
-        "interests": [
-          "biking"
-        ],
-        "availability": [
-          ";08:00-9:00;10:00-13:00", "", "", "", "", "", ""
-        ],
-        "relation": []
+        id: "4",
+        name: "Person 4",
+        email: "123@gmail.com",
+        skills: ["C"],
+        interests: ["biking"],
+        availability: [";08:00-9:00;10:00-13:00", "", "", "", "", "", ""],
+        relation: [],
       },
       {
-        "id": "6",
-        "name": "Person 4",
-        "email": "123@gmail.com",
-        "skills": [
-          "C"
-        ],
-        "interests": [
-          "biking"
-        ],
-        "availability": [
-          ";08:00-9:00;10:00-13:00", "", "", "", "", "", ""
-        ],
-        "relation": []
+        id: "5",
+        name: "Person 4",
+        email: "123@gmail.com",
+        skills: ["C"],
+        interests: ["biking"],
+        availability: [";08:00-9:00;10:00-13:00", "", "", "", "", "", ""],
+        relation: [],
       },
       {
-        "id": "7",
-        "name": "Person 4",
-        "email": "123@gmail.com",
-        "skills": [
-          "C"
-        ],
-        "interests": [
-          "biking"
-        ],
-        "availability": [
-          ";08:00-9:00;10:00-13:00", "", "", "", "", "", ""
-        ],
-        "relation": []
+        id: "6",
+        name: "Person 4",
+        email: "123@gmail.com",
+        skills: ["C"],
+        interests: ["biking"],
+        availability: [";08:00-9:00;10:00-13:00", "", "", "", "", "", ""],
+        relation: [],
       },
       {
-        "id": "8",
-        "name": "Person 4",
-        "email": "123@gmail.com",
-        "skills": [
-          "C"
-        ],
-        "interests": [
-          "biking"
-        ],
-        "availability": [
-          ";08:00-9:00;10:00-13:00", "", "", "", "", "", ""
-        ],
-        "relation": []
-      }
+        id: "7",
+        name: "Person 4",
+        email: "123@gmail.com",
+        skills: ["C"],
+        interests: ["biking"],
+        availability: [";08:00-9:00;10:00-13:00", "", "", "", "", "", ""],
+        relation: [],
+      },
+      {
+        id: "8",
+        name: "Person 4",
+        email: "123@gmail.com",
+        skills: ["C"],
+        interests: ["biking"],
+        availability: [";08:00-9:00;10:00-13:00", "", "", "", "", "", ""],
+        relation: [],
+      },
     ]);
-  }
-  else {
+  } else {
     console.log("profile already set");
   }
   console.log("profiles: " + JSON.stringify(profiles));
 
   const heart = (id: string) => {
-    if (relation.includes(id))
-      relation.splice(relation.indexOf(id), 1);
-    else
-      relation.push(id);
+    if (relation.includes(id)) relation.splice(relation.indexOf(id), 1);
+    else relation.push(id);
     forceUpdate();
-  }
+  };
+
+  const [num, setNum] = useState<number | null>(null);
+  const [input, setInput] = useState<string>("");
+  const [list, setList] = useState<string[]>([]);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setNum(parseInt(input));
+    setInput("");
+  };
+
+  React.useEffect(() => {
+    if (num !== null) {
+      const newList: string[] = [];
+      for (let i = 1; i <= num; i++) {
+        newList.push(`String number ${i}`);
+      }
+      setList(newList);
+    }
+  }, [num]);
 
   return (
     <MainLayout user={user} onLogout={onLogout}>
       <ClassPageNavBar user={user} onLogout={onLogout} />
-      <table className="profiles-container">
-        <tbody>
-          {profiles.map(profile => (
-            <tr className="profile-card" key={profile.id}>
-              <td className="profile-head">
-                <div className="profile-name">
-                  {profile.name}
-                </div>
-                <div className="profile-email">
-                  {profile.email}
-                </div>
-              </td>
-              <td>
+      <div className="class-page-people">
+        <div className="group-form-box">
+          <form onSubmit={handleSubmit}>
+            <div>Grouping</div>
+            <input
+              type="number"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </form>
+          {list.map((item, index) => (
+            <p key={index}>{item}</p>
+          ))}
+        </div>
+        <div className="profiles-container">
+          {profiles.map((profile) => (
+            <div className="profile-card" key={profile.id}>
+              <div className="profile-head">
+                <div className="profile-name">{profile.name}</div>
+                <div className="profile-email">{profile.email}</div>
+              </div>
+              <div>
                 <div className="section-title">Skills</div>
-                {profile.skills.map(skill => (
+                {profile.skills.map((skill) => (
                   <div className="section-content">{skill}</div>
                 ))}
                 <div className="placeholder"></div>
-              </td>
-              <td>
+              </div>
+              <div>
                 <div className="section-title">Interests</div>
-                {profile.interests.map(interest => (
+                {profile.interests.map((interest) => (
                   <div className="section-content">{interest}</div>
                 ))}
                 <div className="placeholder"></div>
-              </td>
-              <td className="profile-tail">
+              </div>
+              <div className="profile-tail">
                 <button className="heart" onClick={() => heart(profile.id)}>
-                  <div >
+                  <div>
                     {relation.includes(profile.id) ? "❤️" : "🤍"}
                     <div className="placeholder"></div>
                   </div>
                 </button>
-              </td>
-            </tr>
+              </div>
+            </div>
           ))}
-        </tbody>
-      </table >
+        </div>
+      </div>
     </MainLayout>
   );
 };
