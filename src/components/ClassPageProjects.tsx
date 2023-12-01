e6d1188104afb59910f0cebf190c0846e0fd1e7d
Henry Mitchell <henry.mitchell0903@gmail.com>
2023-11-30 21:01:16 -0500

    Popout not currently rendered.

--- src/components/ClassPageProjects.tsx
+++ src/components/ClassPageProjects.tsx
@@ -5,6 +5,7 @@ import { useLocation, useParams } from "react-router-dom";
 import "./Projects.css";
 
 import MainLayout from "./MainLayout";
+import PopoutComponent from "./PopoutComponent";
 import ClassPageNavBar from "./ClassPageNavBar";
 import "./ClassPage.css";
 import "./ClassPageNavBar.css";
@@ -20,7 +21,7 @@ interface Project {
   ownerEmail: string;
   title: string;
   description: string;
-  group: {name: string, members: string[]};
+  group: { name: string; members: string[] };
 }
 
 interface ClassPageProjectsProps {
@@ -32,7 +33,6 @@ const ClassPageProjects: React.FC<ClassPageProjectsProps> = ({
   user,
   onLogout,
 }) => {
-
   const location = useLocation();
   const subtitle = location.state.subtitle || "Class Name";
 
@@ -47,58 +47,63 @@ const ClassPageProjects: React.FC<ClassPageProjectsProps> = ({
   const [animate, setAnimate] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>("");
 
-
-
   useEffect(() => {
-      setAnimate(true);
-      console.log("classID: ", classID);
-      if (user?.email) {
-        axios
-          .post("/api/getProjects", {
-            classID: classID,
-          })
-          .then((res) => {
-            setProjects(res.data);
-          })
-          .catch((error) => {
-            console.error(`Error fetching projects: ${error.message}`);
-          });
-      }
-    }, []);
+    setAnimate(true);
+    console.log("classID: ", classID);
+    if (user?.email) {
+      axios
+        .post("/api/getProjects", {
+          classID: classID,
+        })
+        .then((res) => {
+          setProjects(res.data);
+        })
+        .catch((error) => {
+          console.error(`Error fetching projects: ${error.message}`);
+        });
+    }
+  }, []);
 
-
-  const handleNewProjectTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleNewProjectTitleChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setNewProjectTitle(e.target.value);
-  }
-  const handleNewProjectDescChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  };
+  const handleNewProjectDescChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setNewProjectDesc(e.target.value);
-  }
+  };
   const handleNewGroupNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewGroupName(e.target.value);
-  }
+  };
 
   const handleProjectDelete = (projectID: string) => {
     if (window.confirm("Are you sure you want to delete this project?")) {
       axios
         .delete("/api/deleteProject", {
-            data: {
-              projectID: projectID,
-              email: user.email,
-              classID: classID,
-          }
+          data: {
+            projectID: projectID,
+            email: user.email,
+            classID: classID,
+          },
         })
         .then((res) => {
-          setProjects(projects.filter((project) => project.projectID !== projectID));
+          setProjects(
+            projects.filter((project) => project.projectID !== projectID)
+          );
           alert(res.data.message);
         })
         .catch((error) => {
-          setErrorMessage(`Error deleting project: ${error.response.data.message}`)
+          setErrorMessage(
+            `Error deleting project: ${error.response.data.message}`
+          );
           setTimeout(() => {
             setErrorMessage("");
           }, 4000);
         });
     }
-  }
+  };
 
   const handleProjectJoin = (projectID: string) => {
     axios
@@ -125,12 +130,14 @@ const ClassPageProjects: React.FC<ClassPageProjectsProps> = ({
         });
       })
       .catch((error) => {
-        setErrorMessage(`Error joining project: ${error.response.data.message}`)
-          setTimeout(() => {
-            setErrorMessage("");
-          }, 6000);
+        setErrorMessage(
+          `Error joining project: ${error.response.data.message}`
+        );
+        setTimeout(() => {
+          setErrorMessage("");
+        }, 6000);
       });
-  }
+  };
 
   const handleProjectLeave = (projectID: string) => {
     axios
@@ -148,7 +155,9 @@ const ClassPageProjects: React.FC<ClassPageProjectsProps> = ({
                 ...project,
                 group: {
                   ...project.group,
-                  members: project.group.members.filter((member) => member !== user.email),
+                  members: project.group.members.filter(
+                    (member) => member !== user.email
+                  ),
                 },
               };
             }
@@ -157,16 +166,22 @@ const ClassPageProjects: React.FC<ClassPageProjectsProps> = ({
         });
       })
       .catch((error) => {
-        setErrorMessage(`Error leaving project: ${error.response.data.message}`)
-          setTimeout(() => {
-            setErrorMessage("");
-          }, 6000);
+        setErrorMessage(
+          `Error leaving project: ${error.response.data.message}`
+        );
+        setTimeout(() => {
+          setErrorMessage("");
+        }, 6000);
       });
-  }
-  
+  };
+
   const handleNewProjectSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (newProjectTitle === "" || newProjectDesc === "" || newGroupName === "") {
+    if (
+      newProjectTitle === "" ||
+      newProjectDesc === "" ||
+      newGroupName === ""
+    ) {
       setErrorMessage("Please fill out all fields");
       setTimeout(() => {
         setErrorMessage("");
@@ -190,97 +205,140 @@ const ClassPageProjects: React.FC<ClassPageProjectsProps> = ({
           alert(res.data.message);
         })
         .catch((error) => {
-          setErrorMessage(`Error creating project: ${error.response.data.message}`)
+          setErrorMessage(
+            `Error creating project: ${error.response.data.message}`
+          );
           setTimeout(() => {
             setErrorMessage("");
           }, 4000);
         });
-
     }
-  }
+  };
 
   const isUserNotAMember = (project: Project) => {
     if (project.group.members.includes(user.email)) {
       return false;
     }
     return true;
-  }
-  
+  };
 
   const isOwner = (project: Project) => {
     if (project.ownerEmail === user.email) {
       return true;
     }
     return false;
-  }
+  };
 
   const canUserLeaveProject = (project: Project) => {
-    return project.group.members.includes(user.email) && project.ownerEmail !== user.email;
+    return (
+      project.group.members.includes(user.email) &&
+      project.ownerEmail !== user.email
+    );
   };
 
   return (
     <MainLayout user={user} onLogout={onLogout}>
       <ClassPageNavBar user={user} onLogout={onLogout} />
       <div className="add-project">
-            <div className="add-project-text">Have an idea? Create a project and start a group here!</div>
+        <div className="add-project-text">
+          Have an idea? Create a project and start a group here!
+        </div>
       </div>
-      <form className={`new-project-form ${animate ? 'animate' : ''}`} onSubmit={handleNewProjectSubmit}>
-              <input
-                type="text"
-                placeholder="New Project Title"
-                value={newProjectTitle}
-                onChange={handleNewProjectTitleChange}
-              />
-            <div className="new-project-description">
-              <input
-                type="text"
-                placeholder="New Project Description"
-                value={newProjectDesc}
-                onChange={handleNewProjectDescChange}
-              />
-            </div>
-            <div className="new-project-group-name">
-              <input
-                type="text"
-                placeholder="New Group Name"
-                value={newGroupName}
-                onChange={handleNewGroupNameChange}
-              />
-            </div>
-            <button className="new-project-button">Create New Project</button>
-            {errorMessage && (
-              <div className="projects-error-message">
-                {errorMessage}
-              </div>
-            )}
+      <form
+        className={`new-project-form ${animate ? "animate" : ""}`}
+        onSubmit={handleNewProjectSubmit}
+      >
+        <input
+          type="text"
+          placeholder="New Project Title"
+          value={newProjectTitle}
+          onChange={handleNewProjectTitleChange}
+        />
+        <div className="new-project-description">
+          <input
+            type="text"
+            placeholder="New Project Description"
+            value={newProjectDesc}
+            onChange={handleNewProjectDescChange}
+          />
+        </div>
+        <div className="new-project-group-name">
+          <input
+            type="text"
+            placeholder="New Group Name"
+            value={newGroupName}
+            onChange={handleNewGroupNameChange}
+          />
+        </div>
+        <button className="new-project-button">Create New Project</button>
+        {errorMessage && (
+          <div className="projects-error-message">{errorMessage}</div>
+        )}
       </form>
       <div className="class-page-projects">
-      
         {Array.isArray(projects) &&
-          projects.map(({ projectID, title, description, group, ownerEmail}) => (
-            <div className="project-card" key = {projectID}>
-              {isOwner({ projectID, title, description, group, ownerEmail}) && (
-                <button className="delete-project-button" onClick={() => handleProjectDelete(projectID)}>Delete</button>
-              )}
-              <div className="title-description">
-                <div className="project-title">{title}</div>
-                <div className="description">{description}</div>
+          projects.map(
+            ({ projectID, title, description, group, ownerEmail }) => (
+              <div className="project-card" key={projectID}>
+                {isOwner({
+                  projectID,
+                  title,
+                  description,
+                  group,
+                  ownerEmail,
+                }) && (
+                  <button
+                    className="delete-project-button"
+                    onClick={() => handleProjectDelete(projectID)}
+                  >
+                    Delete
+                  </button>
+                )}
+                <div className="title-description">
+                  <div className="project-title">{title}</div>
+                  <div className="description">{description}</div>
+                </div>
+                <div className="group-name">{group.name}</div>
+                <div className="project-members">
+                  Members:{" "}
+                  <span className="project-membersCount">
+                    {group.members.length}
+                  </span>
+                </div>
+                <div className="join-leave-buttons">
+                  {!(user.email == ownerEmail) &&
+                    isUserNotAMember({
+                      projectID,
+                      ownerEmail,
+                      title,
+                      description,
+                      group,
+                    }) && (
+                      <button
+                        className="join-project-button"
+                        onClick={() => handleProjectJoin(projectID)}
+                      >
+                        Join
+                      </button>
+                    )}
+                  {canUserLeaveProject({
+                    projectID,
+                    ownerEmail,
+                    title,
+                    description,
+                    group,
+                  }) && (
+                    <button
+                      className="leave-project-button"
+                      onClick={() => handleProjectLeave(projectID)}
+                    >
+                      Leave
+                    </button>
+                  )}
+                </div>
               </div>
-              <div className="group-name">{group.name}</div>
-              <div className="project-members">
-                Members: <span className="project-membersCount">{group.members.length}</span>
-              </div>
-              <div className="join-leave-buttons">
-              {(!(user.email == ownerEmail) && isUserNotAMember({projectID, ownerEmail, title, description, group})) && (
-              <button className="join-project-button" onClick={() => handleProjectJoin(projectID)}>Join</button>
-              )}
-              {canUserLeaveProject({projectID, ownerEmail, title, description, group}) && (
-                <button className="leave-project-button" onClick={() => handleProjectLeave(projectID)}>Leave</button>
-              )}
-              </div>
-            </div>
-          ))}
-
+            )
+          )}
       </div>
     </MainLayout>
   );
---
+++ src/components/PopoutComponent.tsx
@@ -0,0 +1,220 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { IconContext } from "react-icons";
+import axios from "axios";
+
+import { BsSliders } from "react-icons/bs";
+import { IoMdClose } from "react-icons/io";
+
+import "./PopoutComponent.css";
+
+interface CheckboxOption {
+  id: string;
+  value: string;
+}
+
+const skills: CheckboxOption[] = [
+  { id: "1", value: "JavaScript" },
+  { id: "2", value: "Python" },
+  // Add more skills here
+];
+
+const interests: CheckboxOption[] = [
+  { id: "1", value: "Web Development" },
+  { id: "2", value: "Machine Learning" },
+  // Add more interests here
+];
+
+interface User {
+  name: string;
+  email: string;
+}
+
+interface PopoutComponent {
+  user: User;
+  classID: string;
+  onLogout: () => void;
+}
+
+const PopoutComponent: React.FC<PopoutComponent> = ({
+  user,
+  classID,
+  onLogout,
+}) => {
+  // Popout visibility toggle. Initiated as hidden (false).
+  const [sidebar, setSidebar] = useState(false);
+  // Hooks for each of the class-specific preferences.
+  const [selectedSkills, setSelectedSkills] = useState<string[]>([]);
+  const [preferredSkillsWeight, setPreferredSkillsWeight] = useState(0);
+  const [selectedInterests, setSelectedInterests] = useState<string[]>([]);
+  const [interestsWeight, setInterestsWeight] = useState(0);
+
+  useEffect(() => {
+    console.log(
+      "Default values: ",
+      selectedSkills,
+      preferredSkillsWeight,
+      selectedInterests,
+      interestsWeight
+    );
+  }, [
+    selectedSkills,
+    preferredSkillsWeight,
+    selectedInterests,
+    interestsWeight,
+  ]);
+
+  useEffect(() => {
+    if (user?.email) {
+      axios
+        .post("/api/getClassPreferences", {
+          email: user.email,
+          classID: classID,
+        })
+        .then((res) => {
+          const preferences = res.data;
+          console.log("Preferences: ", preferences);
+          Array.isArray(preferences.preferredSkills) &&
+            setSelectedSkills(preferences.preferredSkills);
+          preferences.preferredSkillsWeight &&
+            setPreferredSkillsWeight(preferences.preferredSkillsWeight);
+          Array.isArray(preferences.interests) &&
+            setSelectedInterests(preferences.interests);
+          preferences.interestsWeight &&
+            setInterestsWeight(preferences.interestsWeight);
+        })
+        .catch((error) => {
+          console.error(`Error fetching preferences: ${error.message}`);
+        });
+    }
+  }, [classID]);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  const handleCheckboxChange = (
+    setFunction: React.Dispatch<React.SetStateAction<string[]>>,
+    selectedValues: string[] = [],
+    value: string
+  ) => {
+    if (selectedValues.includes(value)) {
+      setFunction(selectedValues.filter((v) => v !== value));
+    } else {
+      setFunction([...selectedValues, value]);
+    }
+  };
+
+  const handleUpdate = async () => {
+    if (user?.email) {
+      axios
+        .post("/api/setClassPreferences", {
+          email: user.email,
+          classID: classID,
+          preferredSkills: selectedSkills,
+          preferredSkillsWeight: preferredSkillsWeight,
+          interests: selectedInterests,
+          interestsWeight: interestsWeight,
+        })
+        .catch((error) => {
+          console.error(`Error updating preferences: ${error.message}`);
+        });
+    }
+  };
+
+  return (
+    <div className={`popout-component ${sidebar ? "background-active" : ""}`}>
+      <IconContext.Provider value={{ color: "white" }}>
+        <div className="pref-navbar">
+          {!sidebar && (
+            <Link to="#" className="menu-bars">
+              <BsSliders className="icon-open" onClick={showSidebar} />
+            </Link>
+          )}
+        </div>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items">
+            <li className="popout-toggle">
+              <Link to="#" className="menu-bars">
+                <IoMdClose onClick={() => setSidebar(false)} />
+              </Link>
+            </li>
+            <li>
+              <div className="nav-text">Preferred Skills</div>
+              {skills.map((skill) => (
+                <div key={skill.id}>
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={
+                        Array.isArray(selectedSkills) &&
+                        selectedSkills.includes(skill.value)
+                      }
+                      onChange={() =>
+                        handleCheckboxChange(
+                          setSelectedSkills,
+                          selectedSkills,
+                          skill.value
+                        )
+                      }
+                    />
+                    {skill.value}
+                  </label>
+                </div>
+              ))}
+            </li>
+            <li>
+              <div className="nav-text">Preferred Skills Weight</div>
+              <input
+                type="range"
+                min="0"
+                max="5"
+                value={preferredSkillsWeight}
+                onChange={(e) =>
+                  setPreferredSkillsWeight(Number(e.target.value))
+                }
+              />
+            </li>
+            <li>
+              <div className="nav-text">Interests</div>
+              {interests.map((interest) => (
+                <div key={interest.id}>
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={
+                        Array.isArray(selectedSkills) &&
+                        selectedInterests.includes(interest.value)
+                      }
+                      onChange={() =>
+                        handleCheckboxChange(
+                          setSelectedInterests,
+                          selectedInterests,
+                          interest.value
+                        )
+                      }
+                    />
+                    {interest.value}
+                  </label>
+                </div>
+              ))}
+            </li>
+            <li>
+              <div className="nav-text">Interests Weight</div>
+              <input
+                type="range"
+                min="0"
+                max="5"
+                value={interestsWeight}
+                onChange={(e) => setInterestsWeight(Number(e.target.value))}
+              />
+            </li>
+            <button className="update-preferenes-button" onClick={handleUpdate}>
+              Update
+            </button>
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </div>
+  );
+};
+
+export default PopoutComponent;
