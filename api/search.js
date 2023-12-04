0413503730bfa2a7da859bbae1aab745124b01b1
Henry Gao <henrygao04@gmail.com>
2023-12-03 23:16:20 -0500

    People page css + get api

--- api/search.js
+++ api/search.js
@@ -4,17 +4,19 @@ const { score } = require("./../src/api/utils/_profile.js");
 
 
 module.exports = async (req, res) => {
-    if (req.method !== 'POST') {
-        return res.status(405).json({ error: 'Method not allowed, please use POST' });
+    if (req.method !== 'GET') {
+        return res.status(405).json({ error: 'Method not allowed, please use GET' });
     }
-    const data = req.body;
+    // console.log("seaarch req: " + JSON.stringify(req.query));
+    const data = req.query;
     const email = data.email;
     const classID = data.classID;
     var id;
     try {
+        // console.log("email: " + email);
         // TODO: email is "" when user refreshes the page
         id = await getUserIdByEmail(email);
-        console.log("id: " + id + " email: " + email);
+        // console.log("id: " + id);
     } catch (error) {
         console.error("Error:", error);
         return res.status(500).send({ message: 'Cannot get id: ' + error });
