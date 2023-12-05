9c191833b4684ec5cc0b4ebedd2726f24cae9b22
alexandraiotzova <100001530+alexandraiotzova@users.noreply.github.com>
2023-12-04 20:48:30 -0500

    Update AddClass.tsx

--- src/components/AddClass.tsx
+++ src/components/AddClass.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useLocation, Outlet } from "react-router-dom";
-
 import AddClassNavBar from "./AddClassNavBar";
 import MainLayout from "./MainLayout";
-
 import "./ClassPage.css";
 
+// Define interfaces for user and class information
 interface User {
   name: string;
   email: string;
@@ -18,21 +17,26 @@ interface Class {
   members: string[];
 }
 
+// Define props for the AddClass component
 interface AddClassProps {
   user: User;
   onLogout: () => void;
 }
 
+// Define the AddClass component
 const AddClass: React.FC<AddClassProps> = ({ user, onLogout }) => {
+  // State hooks to manage classes, new class information, form visibility, and error messages
   const [classes, setClasses] = useState<Class[]>([]);
   const [newClassTitle, setNewClassTitle] = useState<string>("");
   const [newClassSubtitle, setNewClassSubtitle] = useState<string>("");
   const [showForm, setShowForm] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>("");
 
+  // React Router hooks for navigation and location
   const navigate = useNavigate();
   const location = useLocation();
-  // Check out the dependency array and mess with it
+
+  // useEffect to handle redirection when on the "/Projects/AddClass" route
   useEffect(() => {
     if (location.pathname === "/Projects/AddClass") {
       navigate("/Projects/AddClass/JoinClass");
@@ -42,14 +46,16 @@ const AddClass: React.FC<AddClassProps> = ({ user, onLogout }) => {
   return (
     <MainLayout user={user} onLogout={onLogout}>
       <div className="add-class-page">
+        {/* Render AddClassNavBar component for navigation */}
         <div className="add-class-page">
           <AddClassNavBar user={user} onLogout={onLogout} />
         </div>
       </div>
+      {/* Render nested routes using the Outlet component */}
       <Outlet />
     </MainLayout>
-    
   );
 };
 
 export default AddClass;
+
