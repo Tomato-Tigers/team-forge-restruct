3b84ba9e2d9becc951428789ba77a42fc5a786a1
alexandraiotzova <100001530+alexandraiotzova@users.noreply.github.com>
2023-12-04 21:03:27 -0500

    Update Register.tsx

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
 import "./Login-Register.css";
 
+// Defining the User interface
 interface User {
   id: number;
   name: string;
@@ -11,53 +11,70 @@ interface User {
   password: string;
 }
 
+// Functional component for user registration
 const Register: React.FC = () => {
+  // State variables to manage form input values
   const [firstName, setFirstName] = useState<string>("");
   const [lastName, setLastName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [confirmPassword, setConfirmPassword] = useState<string>("");
 
+  // Hook to enable programmatic navigation
   const navigate = useNavigate();
 
+  // Function to redirect to the login page
   const redirectToLogin = () => {
     navigate("/");
   };
 
+  // Event handler for first name input change
   const handleFirstNameChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
     setFirstName(event.target.value);
   };
 
-  const handleLastNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  // Event handler for last name input change
+  const handleLastNameChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setLastName(event.target.value);
   };
 
+  // Event handler for email input change
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
   };
 
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  // Event handler for password input change
+  const handlePasswordChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setPassword(event.target.value);
   };
 
+  // Event handler for confirm password input change
   const handleConfirmPasswordChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
     setConfirmPassword(event.target.value);
   };
 
+  // Event handler for form submission
   const handleRegistration = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
+    // Checking if passwords match
     if (password !== confirmPassword) {
       alert("Passwords do not match.");
       return;
     }
 
+    // Constructing the user's full name
     const name = `${firstName} ${lastName}`;
 
+    // Making a POST request to register the user
     axios
       .post("/api/register", {
         name: name,
@@ -69,6 +86,7 @@ const Register: React.FC = () => {
         redirectToLogin();
       })
       .catch((error) => {
+        // Handling registration errors
         if (error.response && error.response.status === 400) {
           alert("Error registering: " + error.response.data);
         } else {
@@ -87,12 +105,14 @@ const Register: React.FC = () => {
           <div className="subtitle">Register</div>
           <div className="form-group">
             <div className="name-group">
+              {/* First name input */}
               <input
                 type="text"
                 placeholder="First name"
                 value={firstName}
                 onChange={handleFirstNameChange}
               />
+              {/* Last name input */}
               <input
                 type="text"
                 placeholder="Last name"
@@ -100,6 +120,7 @@ const Register: React.FC = () => {
                 onChange={handleLastNameChange}
               />
             </div>
+            {/* Email input */}
             <input
               type="email"
               placeholder="Email"
@@ -108,6 +129,7 @@ const Register: React.FC = () => {
             />
           </div>
           <div className="form-group">
+            {/* Password input */}
             <input
               type="password"
               placeholder="Password"
@@ -116,6 +138,7 @@ const Register: React.FC = () => {
             />
           </div>
           <div className="form-group">
+            {/* Confirm password input */}
             <input
               type="password"
               placeholder="Confirm Password"
@@ -124,12 +147,14 @@ const Register: React.FC = () => {
             />
           </div>
           <footer>
+            {/* Link to login page */}
             <div className="login_bottom_left">
               Already have an account?{" "}
               <span className="link" onClick={redirectToLogin}>
                 Login here.
               </span>
             </div>
+            {/* Registration button */}
             <div className="login_bottom_right">
               <button className="login_redirect_button" type="submit">
                 Register
