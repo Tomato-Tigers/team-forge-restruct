43b38c16ecfcab8f12bc8eba345070941e5f1d0d
alexandraiotzova <100001530+alexandraiotzova@users.noreply.github.com>
2023-12-04 20:40:15 -0500

    frontend documentation

---
+++ src/components/what/Projects.tsx
@@ -0,0 +1,97 @@
+import { useState, useEffect } from "react";
+import "./Projects.css";
+import { Link, Routes, Route, Outlet } from "react-router-dom";
+import axios from "axios";
+
+import MainLayout from "./MainLayout";
+import ClassPage from "./ClassPage";
+import AddClass from "./AddClass";
+
+interface User {
+  name: string;
+  email: string;
+}
+
+interface Class {
+  classID: string;
+  title: string;
+  subtitle: string;
+  members: string[];
+}
+
+interface ProjectsProps {
+  user: User;
+  onLogout: () => void;
+}
+
+const Projects: React.FC<ProjectsProps> = ({ user, onLogout }) => {
+  const [classes, setClasses] = useState<Class[]>([]);
+  useEffect(() => {
+    if (user?.email) {
+      // replace for user.token
+      axios
+        .post("/api/getClasses", {
+          email: user?.email,
+        })
+        .then((res) => {
+          setClasses(res.data);
+        })
+        .catch((error) => {
+          // alert(`Error fetching classes: ${error.message}`);
+          console.error(`Error fetching classes: ${error.message}`);
+        });
+    }
+  }, []);
+  return (
+    <MainLayout user={user} onLogout={onLogout}>
+      <div className="projects-container">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                {Array.isArray(classes) &&
+                  classes.map(({ classID, title, subtitle, members }) => (
+                    <Link
+                      to={`./${classID}`}
+                      state={{ subtitle: subtitle }}
+                      style={{ textDecoration: "none" }}
+                      key={classID}
+                    >
+                      <div className="class-card">
+                        <div className="title-subtitle">
+                          <div className="class-title">{title}</div>
+                          <div className="subtitle">{subtitle}</div>
+                        </div>
+                        <div className="members">
+                          Members:{" "}
+                          <span className="membersCount">{members.length}</span>
+                        </div>
+                      </div>
+                    </Link>
+                  ))}
+                <Link
+                  to="/Projects/AddClass"
+                  style={{ textDecoration: "none" }}
+                >
+                  <button className="add-button">+</button>
+                </Link>
+              </>
+            }
+          />
+          <Route
+            path="/Projects/AddClass/*"
+            element={<AddClass user={user} onLogout={onLogout} />}
+          />
+          <Route
+            path="/Projects/:classID/*"
+            element={<ClassPage user={user} onLogout={onLogout} />}
+          />
+        </Routes>
+        <Outlet />
+      </div>
+    </MainLayout>
+  );
+};
+
+export default Projects;
---
+++ src/components/what/Register.tsx
@@ -0,0 +1,145 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import "./Login-Register.css";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+}
+
+const Register: React.FC = () => {
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+
+  const navigate = useNavigate();
+
+  const redirectToLogin = () => {
+    navigate("/");
+  };
+
+  const handleFirstNameChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLastName(event.target.value);
+  };
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleRegistration = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match.");
+      return;
+    }
+
+    const name = `${firstName} ${lastName}`;
+
+    axios
+      .post("/api/register", {
+        name: name,
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        console.log("User registered successfully:", response.data);
+        redirectToLogin();
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 400) {
+          alert("Error registering: " + error.response.data);
+        } else {
+          console.error(`Error registering user: ${error}`);
+        }
+      });
+  };
+
+  return (
+    <div className="register_page" id="register">
+      <div className="login_box">
+        <div className="login_header">
+          <span className="blue_text">Team</span>Forge
+        </div>
+        <form className="login_form" onSubmit={handleRegistration}>
+          <div className="subtitle">Register</div>
+          <div className="form-group">
+            <div className="name-group">
+              <input
+                type="text"
+                placeholder="First name"
+                value={firstName}
+                onChange={handleFirstNameChange}
+              />
+              <input
+                type="text"
+                placeholder="Last name"
+                value={lastName}
+                onChange={handleLastNameChange}
+              />
+            </div>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+            />
+          </div>
+          <footer>
+            <div className="login_bottom_left">
+              Already have an account?{" "}
+              <span className="link" onClick={redirectToLogin}>
+                Login here.
+              </span>
+            </div>
+            <div className="login_bottom_right">
+              <button className="login_redirect_button" type="submit">
+                Register
+              </button>
+            </div>
+          </footer>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
