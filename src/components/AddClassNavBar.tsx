17cdbcfad46ece720aaf89f57cd5b999c11cf18a
alexandraiotzova <100001530+alexandraiotzova@users.noreply.github.com>
2023-12-04 20:49:06 -0500

    Update AddClassNavBar.tsx

--- src/components/AddClassNavBar.tsx
+++ src/components/AddClassNavBar.tsx
@@ -1,27 +1,30 @@
 import React, { useState } from "react";
 import { useParams, Link, Routes, Route } from "react-router-dom";
-
 import "./ClassPageNavBar.css";
 
-import JoinClass from "./JoinClass";
-import CreateClass from "./CreateClass";
-
+// Define interfaces for user information
 interface User {
   name: string;
   email: string;
 }
 
+// Define props for the AddClassNavBar component
 interface AddClassNavBarProps {
   user: User;
   onLogout: () => void;
 }
 
+// Define the AddClassNavBar component
 const AddClassNavBar: React.FC<AddClassNavBarProps> = ({ user, onLogout }) => {
+  // Extract the 'id' parameter from the current route using useParams hook
   const { id } = useParams<{ id: string }>();
+
   return (
     <>
       <div className="navigation">
+        {/* Display the title for the Add Class section */}
         <h1>Add Class</h1>
+        {/* Create navigation links for Join Class and Create Class routes */}
         <Link className="class-page-nav-item" to={"/./Projects/AddClass/JoinClass"}>
           Join Class
         </Link>
